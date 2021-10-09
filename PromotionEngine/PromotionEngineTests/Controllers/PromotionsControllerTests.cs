77dfacb3682ec7216adc53748b25eb72fb6dd74d
navewow <nave.wow@gmail.com>
2021-10-09 19:12:29 +0530

    TestProject

---
+++ PromotionEngine/PromotionEngineTests/Controllers/PromotionsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Controllers.Tests
+{
+    [TestClass()]
+    public class PromotionsControllerTests
+    {
+        [TestMethod()]
+        public void ProcessPromotionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
